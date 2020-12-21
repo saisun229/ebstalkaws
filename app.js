01086bb836b222b1b6582fa38859f33b4f1b9784
Sai krishna reddy Mudhiganti <saikrishnareddymudhiganti@Sais-MacBook-Pro.local>
2020-12-21 17:07:52 -0500

    add env port

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', (req, res) => {
    res.send("welcome to home page");
 })
 
-app.listen(3000, ()=> {
+const port = process.env.port || 3000;
+app.listen(port, ()=> {
     console.log("wassup");
 })
